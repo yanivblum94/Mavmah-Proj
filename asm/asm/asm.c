ae540437dfeee70e1fa0d85d57a736936416c443
Yaniv Blum <jungob94@gmail.com>
2020-12-05 13:34:09 +0200

    includes and defines

---
+++ asm/asm/asm.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define CMDBITS 20; //number of bits in a command
+#define REGRBITS 32; //number of bits in a register
+#define LINELEN 500; //length of max length
+#define MAXLABEL 50; //length of max label
+
+
+
