a6260cb250a2aadad5f6b23f4c05b06282e12681
Yaniv Blum <jungob94@gmail.com>
2020-12-05 14:30:24 +0200

    Binary to decimal

--- asm/asm/asm.c
+++ asm/asm/asm.c
@@ -10,4 +10,34 @@
 #define MAXLABEL 50; //length of max label
 
 
+const char hex_vals[16][1] = { "0","1","2","3","4","5","6","7" ,"8","9","A","B","C","D","E","F" };
 
+int string_length(char s[])//returns the length of a char array
+{
+	int res = 0;
+
+	while (s[res] != '\0')
+		res++;
+
+	return res;
+}
+
+int binary_to_decimal(char n[])//converts a binary numberin char to decimal int
+{
+	int i, res, mul = 0;
+
+	for (res = 0, i = string_length(n) - 1; i >= 0; --i, ++mul)
+		res += (n[i] - 48)*(1 << mul);
+
+	return res;
+}
+
+
+
+
+void  main() {
+	char* bin = "11111110";
+	printf("%d", binary_to_decimal(bin));
+
+
+}
