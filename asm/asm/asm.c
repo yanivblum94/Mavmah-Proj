f12314ce995b977b1eab120dd5bafb3b63ca27f6
Yaniv Blum <jungob94@gmail.com>
2020-12-20 16:59:55 +0200

    Finished coding, needs to debug

--- asm/asm/asm.c
+++ asm/asm/asm.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+#include <ctype.h>
 
 #define CMDBITS 20 //number of bits in a command
 #define REGRBITS 32 //number of bits in a register
@@ -12,6 +13,7 @@
 #define MAXLABEL 50 //length of max label
 #define REGSNUM 16 // number if registers
 #define OPCODESNUM 22// number of opcodes
+#define MEMSIZE 4096
 /*
 Register Number Register Name Purpose
 0 $zero Constant zero
@@ -57,8 +59,8 @@ Opcode Number Name Meaning
 21 halt Halt execution, exit simulator
 */
 
-
-const static char hex_vals[16][1] = { "0","1","2","3","4","5","6","7" ,"8","9","A","B","C","D","E","F" };
+static char dmem[MEMSIZE][8] = { NULL };
+const static char hex_vals[22][2] = { "0","1","2","3","4","5","6","7" ,"8","9","A","B","C","D","E","F", "10", "11", "12", "13", "14", "15" };
 const static char  registers[REGSNUM][5] = { "$zero", "$imm", "$v0 ", "$a0", "$a1", "$t0", "$t1", "$t2", "$t3", "$s0", "$s1","$s2", "$gp ", "$sp", "$fp", "$ra" };
 const static char opcodes[OPCODESNUM][4] = { "add", "sub", "and", "or", "xor", "mul", "sll", "sra", "srl", "beq", "bne", "blt", "bgt", "ble", "bge", "jal", "lw", "sw", "reti", "in", "out", "halt" };
 
@@ -80,24 +82,22 @@ int get_opcode_num(char *opcode) {//receives an opcode and returns its decimal v
 	return -1;
 }
 
-int string_length(char s[])//returns the length of a char array
-{
-	int res = 0;
-
-	while (s[res] != '\0')
-		res++;
-
-	return res;
+//check if the const value is in hex rep.
+bool is_hex(char* value) {
+	if (strlen(value) > 2 && (value[1] == 'x' || value[1] == 'X') && value[0] == '0')//we are in Hex rep.
+		return true;
+	return false;
 }
 
-int binary_to_decimal(char n[])//converts a binary numberin char to decimal int
+int dec_from_string(char* str)//get decimal value of a string
 {
-	int i, res, mul = 0;
+	if (is_hex(str))
+	{
+		return (int)strtol(str + 2, NULL, 16);
+	}
 
-	for (res = 0, i = string_length(n) - 1; i >= 0; --i, ++mul)
-		res += (n[i] - 48)*(1 << mul);
-
-	return res;
+	// we with decimal value
+	return atoi(str);
 }
 
 void remove_last_char(char* str) {//remove ':' from the label
@@ -126,6 +126,32 @@ int get_label_pc(char *label, char label_table[1024][2][MAXLABEL], int labels_nu
 	return -1;
 }
 
+void write_dmemin(char* file_name) {
+	int i = 0;
+	FILE *imem_file = fopen(file_name, "w");
+	if (imem_file == NULL) {
+		fprintf(stderr, "Can't open output file \n");
+		exit(1);
+	}
+	for (i = 0; i < MEMSIZE; i++) {
+		if (dmem[i] == NULL) {
+			fprintf(imem_file, "%s\n", "00000000");
+		}
+		else {
+			fprintf(imem_file, "%s\n", dmem[i]);
+		}
+	}
+	fclose(imem_file);
+}
+
+
+handle_word_cmd(char* address, char* data) {
+	int index, val;
+	index = dec_from_string(address);
+	val = dec_from_string(data);
+	sprintf(dmem[index], "%08X", val);
+}
+
 
 
 int main(int argc, char** argv) {
@@ -134,7 +160,7 @@ int main(int argc, char** argv) {
 	char delim[] = " ,\t\n";//the characters that break the line
 	char label_table[1024][2][MAXLABEL];//max 1024 labels, 2 options for the name(pc and name), max len of label
 	char pc_as_str[20];//the address of the pc as a string
-	FILE *input;
+	FILE *input, *output;
 	input = fopen(argv[1], "r");
 	if (input == NULL) {
 		fprintf(stderr, "Can't open input file \n");
@@ -157,7 +183,7 @@ int main(int argc, char** argv) {
 				}
 				if (ptr != NULL) {//check if it is an only label line
 					if (strcmp(ptr, ".word") == 0) {// we have a .word cmd - skip to next line
-						ptr = strtok(NULL, "\n");
+						break;
 					}
 					else {//regular cmd line 
 						pc++;
@@ -170,6 +196,7 @@ int main(int argc, char** argv) {
 				//check if we need immediate
 				if (!strcmp(ptr, "$imm")) {
 					pc++;
+					break;// I wan't to make sure I only increase PC once
 				}
 				ptr = strtok(NULL, delim);// move to next word
 				if (ptr != NULL) {
@@ -180,9 +207,73 @@ int main(int argc, char** argv) {
 		}
 	}
 	rewind(input);
+	output = fopen(argv[2], "w");
+	if (output == NULL) {
+		fprintf(stderr, "Can't open output file \n");
+		exit(1);
+	}
 	//2nd iteration over the code
-
+	while (fgets(line, LINELEN, input) != NULL) {
+		ptr = strtok(line, delim);
+		bool is_first = true, has_imm=false;
+		while (ptr != NULL) {
+			if (is_first) {// if we are in the first word in the line 
+				char *label = check_label(ptr);
+				if (label != NULL) {//if we have a label
+					ptr = strtok(NULL, delim);//move to the next word
+					if (ptr == NULL || ptr[0] == '#') {// check if it an only label line
+						break;
+					}
+					is_first = false;
+				}
+			}
+			if (!strcmp(ptr, ".word")) {//we have a .word cmd
+				ptr = strtok(NULL, delim);
+				char *address = ptr;
+				ptr = strtok(NULL, delim);
+				char *data = ptr;
+				handle_word_cmd(address, data);
+				break;
+			}
+			//now we have a  pointer to a valid cmd
+			int temp = get_opcode_num(ptr);
+			fputs(hex_vals[temp], output);// enter opcode
+			ptr = strtok(NULL, delim);//move to next word
+			temp = get_reg_num(ptr);// get rd
+			if (temp == 1)
+				has_imm = true;
+			fputs(hex_vals[temp], output);// enter rd
+			ptr = strtok(NULL, delim);//move to next word
+			temp = get_reg_num(ptr);
+			if (temp == 1)
+				has_imm = true;
+			fputs(hex_vals[temp], output);// enter rs
+			ptr = strtok(NULL, delim);//move to next word
+			temp = get_reg_num(ptr);
+			if (temp == 1)
+				has_imm = true;
+			fputs(hex_vals[temp], output);// enter rt
+			fputs("\n", output);//down one line
+			ptr = strtok(NULL, delim);//move to next word
+			if (has_imm) {// check what to do with the imm value
+				int label_pc = get_label_pc(ptr, label_table, labels_num);
+				char imm_val[20];
+				if (label_pc >= 0) {	//we have a label
+					sprintf(imm_val, "%08X", dec_from_string(label_table[label_pc][0]));
+					fputs(imm_val, output);
+				}
+				else {
+					sprintf(imm_val, "%08X", dec_from_string(ptr));
+					fputs(imm_val, output);
+				}
+				fputs("\n", output);//down one line
+			}
+			break;
+		}
+	}
+	write_dmemin(argv[3]);
 	fclose(input);
+	fclose(output);
 	return 1;
 }
 
