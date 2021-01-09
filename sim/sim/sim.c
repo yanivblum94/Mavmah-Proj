681c2be3fdb5de2eab7c9e38d2aea847966a538e
Yaniv Blum <jungob94@gmail.com>
2021-01-09 16:48:27 +0200

    fixed leds

--- sim/sim/sim.c
+++ sim/sim/sim.c
@@ -190,6 +190,9 @@ void write_hwRegTrace(char cmd, int ioReg, int value) {
 	//char reg_name[HWREG_MAX_LENGTH] = hwReg[ioReg];
 	get_hex_from_int(value, 8, temp);
 	printf("temp = %s \n", temp);
+	if (ioReg == 9 && cmd == 'w') {//update leds file
+		fprintf(leds_file, "%d %08X\n", hw_regs[8], hw_regs[9]);
+	}
 	switch (cmd) {
 	case 'w':
 		fprintf(hwRegTraceFile, "%d %s %s %s\n", hw_regs[8] , "WRITE", hwReg[ioReg], temp);
@@ -247,11 +250,6 @@ void write_regout(char* file_name) {//write the regout output file
 		fprintf(regout, "%08X\n", proc_regs[i]);
 	}
 	fclose(regout);
-}
-
-void update_leds(int index) {
-	fprintf(leds_file, "%d %08X\n", hw_regs[8], proc_regs[index]);
-
 }
 
 void timer_handler() {
@@ -548,9 +546,6 @@ bool handle_cmd(int pc_index, bool is_imm) { // returns True if branch command a
 	if (op_num == 20) {//out
 		hw_regs[proc_regs[rs_num + rt_num]] = proc_regs[rd_num];
 		write_hwRegTrace('w', proc_regs[rs_num + rt_num], proc_regs[rd_num]);
-		if (rs_num + rt_num == 9) {
-			update_leds(rd_num);
-		}
 		return false;
 	}
 
